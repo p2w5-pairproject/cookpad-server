abbbccad31f06a8e01f052abfdc5072640173c74
Hani Alfiyyah <hanii.alfiyyah@gmail.com>
2021-01-07 21:06:20 +0700

    add register router work

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,10 @@
 const router = require('express').Router()
+const userRouter = require('./user')
 
 router.get('/', (req, res) => {
     res.send('Welcome to Cookpad!')
 })
+
+router.use(userRouter)
 
 module.exports = router
